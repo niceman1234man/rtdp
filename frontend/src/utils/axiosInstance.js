bba33916428cc8554006444eda93c4adac62ce42
niceman1234man <yihunietarekegn18@gmail.com>
2025-12-30 09:54:38 -0800

    rtdp

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -5,34 +5,9 @@ const BASE_URL = "https://rtdp.onrender.com";
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 30000,
-  // Do not set a global Content-Type so multipart/form-data requests can let the browser set boundaries
-});
-
-// Ensure FormData requests don't carry a JSON content-type header
-axiosInstance.interceptors.request.use((config) => {
-  try {
-    // remove Content-Type if body is FormData so browser sets correct multipart boundary
-    if (config.data && typeof FormData !== 'undefined' && config.data instanceof FormData) {
-      if (config.headers) delete config.headers['Content-Type'];
-    }
-  } catch (e) {
-    // ignore
-  }
-  return config;
-});
-
-// Attach Authorization header from localStorage if present
-axiosInstance.interceptors.request.use((config) => {
-  try {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers = config.headers || {};
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  } catch (e) {
-    // ignore
-  }
-  return config;
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
