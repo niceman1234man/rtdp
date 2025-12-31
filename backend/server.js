557eaa5d615c761bab8525d295dbd5250b16e067
niceman1234man <yihunietarekegn18@gmail.com>
2025-12-31 10:21:53 -0800

    rtdp

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,7 @@ app.use(
   cors({
     origin: ["https://rtdp.netlify.app"],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+    
   })
 );
 app.use(express.json())
