480252acfc98d8bcddd56ffa2f42a7ccb9a7fe07
niceman1234man <yihunietarekegn18@gmail.com>
2025-12-31 12:19:53 -0800

    rtdp

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }))
 /* =======================
    AUTH
 ======================= */
-app.use(authMiddleware)
+// app.use(authMiddleware)
 
 /* =======================
    ROUTES
